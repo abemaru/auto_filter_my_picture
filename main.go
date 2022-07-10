5ae9e11af579b89ade909f9e0513940e1b796d79
L3dlightening <ch4nge.lightening@gmail.com>
2022-07-11 01:43:44 +0900

    add reading picture

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
 )
 
 func main() {
-	if err := os.MkdirAll("case/case01", os.ModePerm); err != nil {
-		log.Fatal(err)
+	// ToDo: Support multiple files
+	fname := "input/PXL_20220702_091647779.jpg"
+
+	f, err := os.Open(fname)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	exif.RegisterParsers(mknote.All...)
+	fmt.Println(f)
+
+	// fileInfo, err := os.Lstat("./")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fileMode := fileInfo.Mode()
+	// unixPerms := fileMode & os.ModePerm
+	// if err := os.MkdirAll("case/case01", unixPerms); err != nil {
+	// 	log.Fatal(err)
+	// }
 }
