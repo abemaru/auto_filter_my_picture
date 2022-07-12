25e5646dbafaddc908c8cea7793b19d6608c4d3f
L3dlightening <ch4nge.lightening@gmail.com>
2022-07-13 01:30:21 +0900

    複数ファイルの読み込みに対応

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/rwcarlsen/goexif/exif"
@@ -10,25 +11,35 @@ import (
 
 func main() {
 	// ToDo: Support multiple files
-	fname := "input/PXL_20220702_091647779.jpg"
-
-	f, err := os.Open(fname)
-
+	files, err := ioutil.ReadDir("input")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	exif.RegisterParsers(mknote.All...)
-	fmt.Println(f)
+	for _, file := range files {
+		fname := "input/" + file.Name()
+		f, err := os.Open(fname)
 
-	// fileInfo, err := os.Lstat("./")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fileMode := fileInfo.Mode()
-	// unixPerms := fileMode & os.ModePerm
-	// if err := os.MkdirAll("case/case01", unixPerms); err != nil {
-	// 	log.Fatal(err)
-	// }
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		exif.RegisterParsers(mknote.All...)
+		fmt.Println(f.Name())
+
+		// ToDo: create file when sorting pictures
+
+		// fileInfo, err := os.Lstat("./")
+		// if err != nil {
+		// 	fmt.Println(err)
+		// }
+		// fileMode := fileInfo.Mode()
+		// unixPerms := fileMode & os.ModePerm
+		// if err := os.MkdirAll("case/case01", unixPerms); err != nil {
+		// 	log.Fatal(err)
+		// }
+
+	}
 }
